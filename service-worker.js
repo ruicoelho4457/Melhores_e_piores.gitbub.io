4ea44c8fd6852615e35d79e4cbdc18fc3ec437fa
ruicoelho4457 <ruicoelho4457@gmail.com>
2026-08-18 16:16:58 +0100

    Add files via upload
    Correção histórico 100

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_NAME = 'crypto-movers-pwa-v1';
+const CACHE_NAME = 'crypto-movers-pwa-v2';
 const APP_SHELL = ['/', '/manifest.webmanifest', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', event => {
